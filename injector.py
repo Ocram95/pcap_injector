50e9a7b828a038bc6957fa08b59ac7e9513fe8b2
Ocram95 <theovation95@gmail.com>
2021-03-25 10:38:54 +0100

    Tool first version

---
+++ injector.py
@@ -0,0 +1,170 @@
+import sys
+import optparse
+import subprocess
+import pandas as pd
+from scapy.utils import rdpcap
+from scapy.utils import wrpcap
+from scapy.all import *
+from scapy.layers.inet6 import IPv6
+
+
+FIELD_LENGTH = {
+	"FL": 20,
+	"TC": 8,
+	"HL": 1
+}
+
+def process_command_line(argv):
+	parser = optparse.OptionParser()
+	parser.add_option(
+		'-r',
+		'--pcap',
+		help='Specify the pcap to inject.',
+		action='store',
+		type='string',
+		dest='pcap')
+
+	parser.add_option(
+		'-f',
+		'--field',
+		help='Specify the field to exploit to contain the payload (i.e., FL, TC, HL).',
+		action='store',
+		type='string',
+		dest='field')
+
+	parser.add_option(
+		'-a',
+		'--attack',
+		help='Specify the attack (i.e., text file, string).',
+		action='store',
+		type='string',
+		dest='attack')
+
+	settings, args = parser.parse_args(argv)
+		
+	if not settings.pcap:
+		raise ValueError("A pcap file must be specified.")
+	if not settings.field:
+		raise ValueError("A field must be specified.")
+	if settings.field not in FIELD_LENGTH:
+		raise ValueError("The specified field is incorrect or not supported.")
+	if not settings.attack:
+		raise ValueError("An attack must be specified.")
+
+
+	return settings, args
+
+
+def read_attack(attack_to_read, dim_field):
+	print("Reading the attack...")
+	try:
+		file = open(attack_to_read, "r")
+		file_in_string = file.read()
+	except IOError:
+		print("This file does not exist. I will inject the string: " + str(attack_to_read))
+		file_in_string = str(attack_to_read)
+	#Conversion of the input in bits
+	attack_in_bits = ''.join(format(ord(bit), '08b') for bit in file_in_string)
+	#Division in chunks of 'dim_field' size
+	attack_in_chunks = [attack_in_bits[i:i+dim_field] for i in range(0, len(attack_in_bits), dim_field)]
+	print("Number of packets needed: " + str(len(attack_in_chunks)))
+	return attack_in_chunks
+
+def find_flows(pcap_to_read, number_packets):
+	print("Creating tmp files...")
+	create_tmp_csv = "tshark -r " + pcap_to_read + " -T fields -e ipv6.flow -e ipv6.src -e ipv6.dst -E header=y -E separator=, > tmp.csv"
+	process = subprocess.Popen(create_tmp_csv, shell=True, stdout=subprocess.PIPE)
+	process.wait()
+	df = pd.read_csv('tmp.csv')
+	df = df.groupby(['ipv6.flow', 'ipv6.src', 'ipv6.dst']).size().to_frame('#pkts').reset_index()
+	delete_tmp_csv = "rm tmp.csv"
+	process = subprocess.Popen(delete_tmp_csv, shell=True, stdout=subprocess.PIPE)
+	process.wait()
+	print("Deleting tmp files...")
+	return df.loc[df['#pkts'] >= number_packets]
+
+def inject(pcap, source, destination, flow_label, targeted_field, attack_in_chunks):
+	print("Reading input pcap. This might takes few minutes...")
+	pkts = rdpcap(pcap)
+	secret_index = 0
+	wire_len = []
+	index = 0
+	print("Injecting...")
+	for x in range(len(pkts)):
+		wire_len.append(pkts[x].wirelen)
+		if secret_index < len(attack_in_chunks):
+			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst and flow_label == pkts[x][IPv6].fl:
+				if targeted_field == "FL":
+					pkts[x][IPv6].fl = int(attack_in_chunks[secret_index],2)
+				elif targeted_field == "TC":
+					pkts[x][IPv6].tc = int(attack_in_chunks[secret_index],2)
+				elif targeted_field == "HL":
+					if int(attack_in_chunks[secret_index],2) == 0:
+						pkts[x][IPv6].hlim = pkts[x][IPv6].hlim + 20
+					else:
+						pkts[x][IPv6].hlim = pkts[x][IPv6].hlim - 20
+				secret_index += 1
+		pkts[x].wirelen = wire_len[index]
+		index += 1
+		wrpcap("injected_" + str(pcap), pkts[x], append=True, linktype=101)
+	print("Injection succesfully finished!")
+	return pkts, wire_len
+
+# def write_pcap(pkts, wire_len, filename):
+# 	index = 0
+# 	print("Writing the final pcap.")
+# 	for pkt in pkts:
+# 		pkt.wirelen = wire_len[index]
+# 		wrpcap("injected2_" + str(filename), pkt, append=True)
+# 		index += 1
+
+
+settings, args = process_command_line(sys.argv)
+attack_in_chunks = read_attack(settings.attack, FIELD_LENGTH[settings.field])
+flows = find_flows(settings.pcap, len(attack_in_chunks))
+if len(flows) > 0:
+	print("CONVERSATIONS FOUND")
+	print(flows)
+	while True:
+		what_flow = int(input("Choose the flow by its index (left column): "))
+		if not what_flow in flows.index:
+			print("Invalid flow index")
+			continue
+		else:
+			source = flows.loc[what_flow]['ipv6.src']
+			destination = flows.loc[what_flow]['ipv6.dst']
+			flow_label_extended = flows.loc[what_flow]['ipv6.flow']					#0x000f92c1
+			flow_label = int(flow_label_extended[:2] + flow_label_extended[5:], 16)	#0xf92c1 -> 1020609
+			break
+	print("Conversation choosen: src = " + str(source) + " dst = " + str(destination) + " flow label = " + str(flow_label))
+else:
+	print("No conversations with enough packets are found in this pcap!")
+
+if len(flows) > 0:
+	injected_pcap, wire_len = inject(settings.pcap, source, destination, flow_label, settings.field, attack_in_chunks)
+	# write_pcap(injected_pcap, wire_len, settings.pcap)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
