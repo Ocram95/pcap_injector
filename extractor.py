6d4fd1524a5932065db556c791ac313b2fe56fd0
Ocram95 <theovation95@gmail.com>
2021-04-09 16:41:19 +0200

    Major fixes

--- extractor.py
+++ extractor.py
@@ -7,6 +7,7 @@ from scapy.utils import wrpcap
 from scapy.all import *
 from scapy.layers.inet6 import IPv6
 import random
+import base64
 
 #Field and length of them supported
 FIELD_LENGTH = {
@@ -38,8 +39,27 @@ def process_command_line(argv):
 		'--packets',
 		help='Specify the number of packets to extract.',
 		action='store',
+		default = 0,
 		type='int',
 		dest='packets')
+
+	parser.add_option(
+		'-b',
+		'--bits',
+		help='Specify the number of bits to extract.',
+		action='store',
+		default = 0,
+		type='int',
+		dest='bits')
+
+	parser.add_option(
+		'-i',
+		'--image',
+		help='Specify to extract an image.',
+		action='store',
+		default = 'n',
+		type='string',
+		dest='image')
 
 	settings, args = parser.parse_args(argv)
 		
@@ -51,6 +71,9 @@ def process_command_line(argv):
 		raise ValueError("The specified field is incorrect or not supported.")
 	if not settings.packets:
 		raise ValueError("The number of packets to extract must be specified.")
+	if settings.image != 'y' and settings.image != 'n':
+		raise ValueError("The image flag can only support value 'y' or 'n'.")
+
 
 	return settings, args
 
@@ -58,13 +81,14 @@ def process_command_line(argv):
 def find_flows(pcap_to_read, number_of_packets):
 	#Creation of csv file where each line is composed of three-tuple src and dst for each packet 
 	print("Creating tmp files...")
-	create_tmp_csv = "tshark -r " + pcap_to_read + " -T fields -e ipv6.src -e ipv6.dst -E header=y -E separator=, > tmp.csv"
+	create_tmp_csv = "tshark -r " + pcap_to_read + " -T fields -e ipv6.src -e ipv6.dst -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e ipv6.nxt -E header=y -E separator=, > tmp.csv"
 	process = subprocess.Popen(create_tmp_csv, shell=True, stdout=subprocess.PIPE)
 	process.wait()
-	#Count of packets that compose each flow, grouping by src and dst (fl is wrong: consider the case 
+	#Count of packets that compose each flow (fl should not be considered: consider the case 
 	#where fl is injected!)
 	df = pd.read_csv('tmp.csv')
-	df = df.groupby(['ipv6.src', 'ipv6.dst']).size().to_frame('#pkts').reset_index()
+	df_tcp = df.groupby(['ipv6.src', 'ipv6.dst', 'tcp.srcport', 'tcp.dstport', 'ipv6.nxt']).size().to_frame('#pkts').reset_index()
+	df_udp = df.groupby(['ipv6.src', 'ipv6.dst', 'udp.srcport', 'udp.dstport', 'ipv6.nxt']).size().to_frame('#pkts').reset_index()
 	#Deleting of csv file
 	delete_tmp_csv = "rm tmp.csv"
 	process = subprocess.Popen(delete_tmp_csv, shell=True, stdout=subprocess.PIPE)
@@ -73,9 +97,11 @@ def find_flows(pcap_to_read, number_of_packets):
 	#Adding INDEX column name
 	df.index.name = "INDEX"
 	#Return flows which contains at leats 'number_of_packets' packets
-	return df.loc[df['#pkts'] >= number_of_packets]
+	df_final = df_tcp.append(df_udp)
+	df_final = df_final[['ipv6.src', 'ipv6.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', 'ipv6.nxt', '#pkts']]
+	return df_final.loc[df_final['#pkts'] >= number_of_packets]
 
-def extract(pcap, source, destination, targeted_field, number_of_packets):
+def extract_packets(pcap, source, destination, src_port, dst_port, protocol, targeted_field, number_of_packets):
 	print("Reading input pcap. This might takes few minutes...")
 	pkts = rdpcap(pcap)
 	secret_index = 0
@@ -83,7 +109,7 @@ def extract(pcap, source, destination, targeted_field, number_of_packets):
 	print("Extracting...")
 	for x in range(len(pkts)):
 		if secret_index < number_of_packets:
-			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst:
+			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst and src_port == pkts[x].sport and dst_port == pkts[x].dport and protocol == pkts[x].nh:
 				if targeted_field == "FL":
 					#Conversion of the fl value into 20 bit value
 					secret_extracted += '{0:020b}'.format(pkts[x][IPv6].fl)
@@ -96,6 +122,45 @@ def extract(pcap, source, destination, targeted_field, number_of_packets):
 						secret_extracted += '0'
 				secret_index += 1
 	#Creation of 8 bit chunks to correctly interpret characters
+	print(secret_extracted)
+	secret_in_chunks = list((secret_extracted[0+i:8+i] for i in range(0, len(secret_extracted), 8)))
+	print(secret_in_chunks)
+	secret_string = ''
+	for i in range(len(secret_in_chunks)):
+		secret_string += chr(int(secret_in_chunks[i],2))
+	print('-' * 25)
+	print("PAYLOAD EXTRACTED")
+	return secret_string
+
+def extract_bits(pcap, source, destination, src_port, dst_port, protocol, targeted_field, number_of_bits):
+	print("Reading input pcap. This might takes few minutes...")
+	pkts = rdpcap(pcap)
+	secret_index = 0
+	secret_extracted = ''
+	print("Extracting...")
+	for x in range(len(pkts)):
+		if secret_index < number_of_bits:
+			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst and src_port == pkts[x].sport and dst_port == pkts[x].dport and protocol == pkts[x].nh:
+				if targeted_field == "FL":
+					#The number of bits of the last packet
+					rest = number_of_bits % 20
+					if number_of_bits - len(secret_extracted) == rest:	#if it is the last packet
+						tmp = '{0:020b}'.format(pkts[x][IPv6].fl)[20 - rest:] #take the last 'rest' bit and don't consider the first bits
+						secret_extracted += tmp
+						secret_index += rest
+					else:
+						secret_extracted += '{0:020b}'.format(pkts[x][IPv6].fl)
+						secret_index += 20
+				elif targeted_field == "TC":
+					secret_extracted += '{0:08b}'.format(pkts[x][IPv6].tc)
+					secret_index += 8
+				elif targeted_field == "HL":
+					if pkts[x][IPv6].hlim == 255:
+						secret_extracted += '1'
+					elif pkts[x][IPv6].hlim == 10:
+						secret_extracted += '0'
+					secret_index += 1
+	#Creation of 8 bit chunks to correctly interpret characters
 	secret_in_chunks = list((secret_extracted[0+i:8+i] for i in range(0, len(secret_extracted), 8)))
 	secret_string = ''
 	for i in range(len(secret_in_chunks)):
@@ -104,16 +169,24 @@ def extract(pcap, source, destination, targeted_field, number_of_packets):
 	print("PAYLOAD EXTRACTED")
 	return secret_string
 
+
 def flow_selection(flows, number):
+	#TODO: extend to more protocols?
 	source = flows.loc[number]['ipv6.src']
 	destination = flows.loc[number]['ipv6.dst']
-	return source, destination
-
+	if not math.isnan(flows.loc[number]['tcp.srcport']):
+		protocol = 6
+		src_port = flows.loc[number]['tcp.srcport']
+		dst_port = flows.loc[number]['tcp.dstport']
+	elif not math.isnan(flows.loc[number]['udp.srcport']):
+		protocol = 17
+		src_port = flows.loc[number]['udp.srcport']
+		dst_port = flows.loc[number]['udp.dstport']
+	return source, destination, int(src_port), int(dst_port), protocol
 
 
 
 settings, args = process_command_line(sys.argv)
-
 flows = find_flows(settings.pcap, settings.packets)
 if len(flows) > 0:
 	print('-' * 25)
@@ -129,13 +202,22 @@ if len(flows) > 0:
 				print("Invalid flow index")
 				continue
 			else:
-				source, destination = flow_selection(flows, what_flow)
+				source, destination, src_port, dst_port, protocol = flow_selection(flows, what_flow)
 				break
 		else:
 			print("This operation is not supported!")
 	print('-' * 25)
-	print("Conversation: src = " + str(source) + " dst = " + str(destination))
-	print(extract(settings.pcap, source, destination, settings.field, settings.packets))
+	print("Wireshark filter: ipv6.src == " + str(source) + " and ipv6.dst == " + str(destination) + " and tcp.srcport == " + str(src_port) + " and tcp.dstport == " + str(dst_port))
+	if settings.bits != 0:
+		payload = extract_bits(settings.pcap, source, destination, src_port, dst_port, protocol, settings.field, settings.bits)
+	else:
+		payload = extract_packets(settings.pcap, source, destination, src_port, dst_port, protocol, settings.field, settings.packets)
+	if settings.image == 'n':
+		print(payload)
+	else: 
+		decodeit = open('extr_image.png', 'wb')
+		decodeit.write(base64.b64decode((payload)))
+		decodeit.close()
 else:
 	print("No conversations are found within this pcap!")
 
--- injector.py
+++ injector.py
@@ -7,6 +7,8 @@ from scapy.utils import wrpcap
 from scapy.all import *
 from scapy.layers.inet6 import IPv6
 import random
+import base64
+import csv
 
 #Field and length of them supported
 FIELD_LENGTH = {
@@ -58,28 +60,31 @@ def process_command_line(argv):
 
 def read_attack(attack_to_read, dim_field):
 	print("Reading the attack...")
-	try:
+	if attack_to_read.endswith('.txt'):
 		file = open(attack_to_read, "r")
 		file_in_string = file.read()
-	except IOError:
-		print("This file does not exist. I will inject the string: " + str(attack_to_read))
+	elif attack_to_read.endswith('.jpg') or attack_to_read.endswith('.png'):
+		with open(attack_to_read, "rb") as image2string:
+			file_in_string = base64.b64encode(image2string.read()).decode("utf-8") 
+	else:
 		file_in_string = str(attack_to_read)
-	#Conversion of the input in bits
 	attack_in_bits = ''.join(format(ord(bit), '08b') for bit in file_in_string)
 	#Division in chunks of 'dim_field' size
 	attack_in_chunks = [attack_in_bits[i:i+dim_field] for i in range(0, len(attack_in_bits), dim_field)]
 	print("Number of packets needed: " + str(len(attack_in_chunks)))
-	return attack_in_chunks
+	print("Number of bits needed: " + str(len(attack_in_bits)))
+	return attack_in_chunks, str(len(attack_in_chunks)), str(len(attack_in_bits))
 
 def find_flows(pcap_to_read, number_packets):
 	#Creation of csv file where each line is composed of three-tuple src and dst for each packet 
 	print("Creating tmp files...")
-	create_tmp_csv = "tshark -r " + pcap_to_read + " -T fields -e ipv6.flow -e ipv6.src -e ipv6.dst -E header=y -E separator=, > tmp.csv"
+	create_tmp_csv = "tshark -r " + pcap_to_read + " -T fields -e ipv6.flow -e ipv6.src -e ipv6.dst -e tcp.srcport -e tcp.dstport -e udp.srcport -e udp.dstport -e ipv6.nxt -E header=y -E separator=, > tmp.csv"
 	process = subprocess.Popen(create_tmp_csv, shell=True, stdout=subprocess.PIPE)
 	process.wait()
 	#Count of packets that compose each flow, grouping by src, dst and fl
 	df = pd.read_csv('tmp.csv')
-	df = df.groupby(['ipv6.flow', 'ipv6.src', 'ipv6.dst']).size().to_frame('#pkts').reset_index()
+	df_tcp = df.groupby(['ipv6.flow', 'ipv6.src', 'ipv6.dst', 'tcp.srcport', 'tcp.dstport', 'ipv6.nxt']).size().to_frame('#pkts').reset_index()
+	df_udp = df.groupby(['ipv6.flow', 'ipv6.src', 'ipv6.dst', 'udp.srcport', 'udp.dstport', 'ipv6.nxt']).size().to_frame('#pkts').reset_index()
 	#Deleting of csv file
 	delete_tmp_csv = "rm tmp.csv"
 	process = subprocess.Popen(delete_tmp_csv, shell=True, stdout=subprocess.PIPE)
@@ -88,9 +93,11 @@ def find_flows(pcap_to_read, number_packets):
 	#Adding INDEX column name
 	df.index.name = "INDEX"
 	#Return flows which contains at leats 'number_of_packets' packets
-	return df.loc[df['#pkts'] >= number_packets]
+	df_final = df_tcp.append(df_udp)
+	df_final = df_final[['ipv6.flow', 'ipv6.src', 'ipv6.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', 'ipv6.nxt', '#pkts']]
+	return df_final.loc[df_final['#pkts'] >= number_packets]
 
-def inject(pcap, source, destination, flow_label, targeted_field, attack_in_chunks):
+def inject(pcap, source, destination, flow_label, src_port, dst_port, protocol, targeted_field, attack_in_chunks, attack):
 	print("Reading input pcap. This might takes few minutes...")
 	pkts = rdpcap(pcap)
 	secret_index = 0
@@ -101,7 +108,7 @@ def inject(pcap, source, destination, flow_label, targeted_field, attack_in_chun
 		wire_len.append(pkts[x].wirelen)
 		if secret_index < len(attack_in_chunks):
 			#Search for the correct flow
-			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst and flow_label == pkts[x][IPv6].fl:
+			if source == pkts[x][IPv6].src and destination == pkts[x][IPv6].dst and flow_label == pkts[x][IPv6].fl and src_port == pkts[x].sport and dst_port == pkts[x].dport and protocol == pkts[x].nh:
 				if targeted_field == "FL":
 					pkts[x][IPv6].fl = int(attack_in_chunks[secret_index],2)
 				elif targeted_field == "TC":
@@ -115,22 +122,38 @@ def inject(pcap, source, destination, flow_label, targeted_field, attack_in_chun
 		pkts[x].wirelen = wire_len[index]
 		index += 1
 		#WARNING: check if the linktype is what is needed
-		wrpcap(str(targeted_field) + "_injected_" + str(pcap), pkts[x], append=True, linktype=101)
+		wrpcap(str(targeted_field) + "_a=" + str(attack) + "_" + str(pcap), pkts[x], append=True, linktype=101)
 	print("Injection succesfully finished!")
 	return pkts, wire_len
 
 def flow_selection(flows, number):
+	#TODO: extend to more protocols?
 	source = flows.loc[number]['ipv6.src']
 	destination = flows.loc[number]['ipv6.dst']
 	flow_label_extended = flows.loc[number]['ipv6.flow']					#0x000f92c1
 	flow_label = int(flow_label_extended[:2] + flow_label_extended[5:], 16)	#0xf92c1 -> 1020609
-	return source, destination, flow_label
+	if not math.isnan(flows.loc[number]['tcp.srcport']):
+		protocol = 6
+		src_port = flows.loc[number]['tcp.srcport']
+		dst_port = flows.loc[number]['tcp.dstport']
+	elif not math.isnan(flows.loc[number]['udp.srcport']):
+		protocol = 17
+		src_port = flows.loc[number]['udp.srcport']
+		dst_port = flows.loc[number]['udp.dstport']
+	return source, destination, flow_label, int(src_port), int(dst_port), protocol
 
+def write_to_csv(csv_file_name, filename, attack, field, src, dst, p_src, p_dst, proto, lengthb, lengthp):
+	csv_exists = os.path.isfile(csv_file_name)
+	with open(csv_file_name, mode='a') as file:
+		writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		if not csv_exists:
+			writer.writerow(['file name', 'attack', 'field','src', 'dst', 'p_src', 'p_dst', 'proto', 'length [packet]', 'length [bit]'])
 
+		writer.writerow([filename, attack, field, src, dst, p_src, p_dst, proto, lengthb, lengthp])
 
 
 settings, args = process_command_line(sys.argv)
-attack_in_chunks = read_attack(settings.attack, FIELD_LENGTH[settings.field])
+attack_in_chunks, lengthp, lengthb = read_attack(settings.attack, FIELD_LENGTH[settings.field])
 flows = find_flows(settings.pcap, len(attack_in_chunks))
 if len(flows) > 0:
 	print('-' * 25)
@@ -145,26 +168,26 @@ if len(flows) > 0:
 				print("Invalid flow index")
 				continue
 			else:
-				source, destination, flow_label = flow_selection(flows, what_flow)
+				source, destination, flow_label, src_port, dst_port, protocol = flow_selection(flows, what_flow)
 				break
 		elif operation == 'r':
 			rnd_flow = random.choice(flows.index.tolist())
-			print('Flow ' + str(rnd_flow) + ' is choosen.')
-			source, destination, flow_label = flow_selection(flows, rnd_flow)
+			print('Flow ' + str(rnd_flow) + ' is chosen.')
+			source, destination, flow_label, src_port, dst_port, protocol = flow_selection(flows, rnd_flow)
 			break
 		elif operation == '':
 			print('First flow is chosen.')
 			first_flow = flows.index.tolist()[0]
-			source, destination, flow_label = flow_selection(flows, first_flow)
+			source, destination, flow_label, src_port, dst_port, protocol = flow_selection(flows, first_flow)
 			break
 		else:
 			print("This operation is not supported!")
 	print('-' * 25)
-	print("Conversation: src = " + str(source) + " dst = " + str(destination) + " flow label = " + str(flow_label))
-	injected_pcap, wire_len = inject(settings.pcap, source, destination, flow_label, settings.field, attack_in_chunks)
+	print("Wireshark filter: ipv6.src == " + str(source) + " and ipv6.dst == " + str(destination) + " and ipv6.flow == " + str(flow_label) + " and tcp.srcport == " + str(src_port) + " and tcp.dstport == " + str(dst_port))
+	injected_pcap, wire_len = inject(settings.pcap, source, destination, flow_label, src_port, dst_port, protocol, settings.field, attack_in_chunks, settings.attack)
+	write_to_csv('injected_flows.csv', settings.pcap, settings.attack, settings.field, source, destination, src_port, dst_port, protocol, lengthp, lengthb)
 else:
 	print("No conversations with enough packets are found in this pcap!")
-
 
 
 
