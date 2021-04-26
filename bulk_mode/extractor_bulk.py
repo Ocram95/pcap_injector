656df239b336d52e4ef6e2bf06f8f952f27ad612
Ocram95 <theovation95@gmail.com>
2021-04-26 15:22:49 +0200

    Bulk mode v0.1

---
+++ bulk_mode/extractor_bulk.py
@@ -0,0 +1,134 @@
+import sys
+import optparse
+import subprocess
+import pandas as pd
+from scapy.utils import rdpcap
+from scapy.utils import wrpcap
+from scapy.all import *
+from scapy.layers.inet6 import IPv6
+import random
+import base64
+import csv
+
+def process_command_line(argv):
+	parser = optparse.OptionParser()
+	parser.add_option(
+		'-r',
+		'--pcap',
+		help='Specify the pcap to read.',
+		action='store',
+		type='string',
+		dest='pcap')
+
+	parser.add_option(
+		'-i',
+		'--injected_flows',
+		help='Specify csv file of the injected flows.',
+		action='store',
+		type='string',
+		dest='injected_flows')
+
+
+	settings, args = parser.parse_args(argv)
+		
+	if not settings.pcap:
+		raise ValueError("A pcap file must be specified.")
+	if not settings.injected_flows:
+		raise ValueError("A csv file with the injected flows must be specified.")
+
+	return settings, args
+
+
+def create_dict_attack(injected_flows):
+	list_of_dict = []
+	with open(injected_flows) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		#Skip header
+		next(csv_reader, None)
+		for row in csv_reader:
+			tmp_dict = {}
+			tmp_dict['target_field'] = row[0]
+			tmp_dict['src'] = row[1]
+			tmp_dict['dst'] = row[2]
+			tmp_dict['psrc'] = int(row[3])
+			tmp_dict['pdst'] = int(row[4])
+			tmp_dict['nxt'] = int(row[5])
+			tmp_dict['plenght'] = int(row[6])
+			tmp_dict['blength'] = int(row[7])
+			tmp_dict['counter'] = 0
+			tmp_dict['extracted'] = ''
+			list_of_dict.append(tmp_dict)
+	
+	return list_of_dict
+
+def extract(pcap, attack_dict):
+	pkts = rdpcap(pcap)
+	for x in range(len(pkts)):
+		if pkts[x][IPv6].nh != 58:
+			p_source, p_destination, p_psrc, p_pdst, p_nxt = pkts[x][IPv6].src, pkts[x][IPv6].dst, pkts[x].sport, pkts[x].dport, pkts[x][IPv6].nh
+			for attack in attack_dict:
+				a_source, a_destination, a_psrc, a_pdst, a_nxt = attack['src'], attack['dst'], attack['psrc'], attack['pdst'], attack['nxt']
+				if p_source == a_source and p_destination == a_destination and p_psrc == a_psrc and p_pdst == a_pdst and p_nxt == a_nxt:
+					if attack['counter'] < attack['blength']:
+						targeted_field = attack['target_field']
+						if targeted_field == "FL":
+							rest = attack['blength'] % 20
+							if attack['blength'] - len(attack['extracted']) == rest:	#if it is the last packet
+								tmp = '{0:020b}'.format(pkts[x][IPv6].fl)[20 - rest:] #take the last 'rest' bit and don't consider the first bits
+								attack['extracted'] += tmp
+								attack['counter'] += rest
+							else:
+								attack['extracted'] += '{0:020b}'.format(pkts[x][IPv6].fl)
+								attack['counter'] += 20
+						elif targeted_field == "TC":
+							attack['extracted'] += '{0:08b}'.format(pkts[x][IPv6].tc)
+							attack['counter'] += 8
+						elif targeted_field == "HL":
+							if pkts[x][IPv6].hlim == 255:
+								attack['extracted'] += '1'
+							elif pkts[x][IPv6].hlim == 10:
+								attack['extracted'] += '0'
+							attack['counter'] += 1
+
+	extracted_attacks_in_chunks = []
+	for attack in attack_dict:
+		extracted_attacks_in_chunks.append((attack['target_field'],list((attack['extracted'][0+i:8+i] for i in range(0, len(attack['extracted']), 8)))))
+	extracted_attacks = []
+	for attack in extracted_attacks_in_chunks:
+		secret_string = ''
+		for chunk in attack[1]:
+			secret_string += chr(int(chunk, 2))
+		extracted_attacks.append((attack[0], secret_string))
+	return extracted_attacks
+
+def write_extracted(attacks):
+	file = open("extracted_attacks.txt", "w")
+	for x in attacks:
+		file.write(x[0] + ', ' + x[1] + '\n')
+
+settings, args = process_command_line(sys.argv)
+attack_dict = create_dict_attack(settings.injected_flows)
+attacks = extract(settings.pcap, attack_dict)
+write_extracted(attacks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
